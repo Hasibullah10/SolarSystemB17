603c08d209659e6cd98fa54bb2d75d61a1402a8c
Hasibullah10 <141285022+Hasibullah10@users.noreply.github.com>
2023-11-02 12:59:12 -0400

    FirstCommitForSolarSystem

---
+++ src/test/java/SolarSystem/Main.java
@@ -0,0 +1,11 @@
+package SolarSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        SolarSystemBasics Solar=new SolarSystemBasics();
+        Solar.Sun="1";
+        Solar.Moon="2";
+        Solar.Stars="3";
+        Solar.Planets="4";
+    }
+}
